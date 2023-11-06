aaa23ce155680b6bc0239a985682670722bea9e4
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2023-11-06 15:11:22 +0100

    define functions

---
+++ README.md
@@ -0,0 +1,18 @@
+# Basic TODO app
+
+## Requirements
+
+1. Add a todo
+2. Remove a todo
+3. Update a todo
+4. List todos
+5. Add a description for a todo
+6. Mark a todo as completed
+7. Move a todo in order
+8. Search for todos by name
+9. Sort todo alphabetically (ascending/descending)
+10. Sort todo by creation date
+11. Create columns (to do, ongoing, done)
+12. Move a todo between columns
+13. Save todos to local storage
+14. Read todos from local storage
---
+++ src/main.ts
@@ -0,0 +1,80 @@
+type Todo = {
+  id: number;
+  name: string;
+  description: string;
+};
+
+const todo = (() => {
+  const todos: Todo[] = [];
+
+  return {
+    /**
+     * Adds a todo with a unique id.
+     * @param name Name of the todo
+     * @param description Description of the todo
+     */
+    addTodo(name: string, description: string): void {
+      todos.push({ id: 1, name, description });
+    },
+
+    /**
+     *
+     * @param id Id of todo to delete
+     */
+    removeTodo(id: number): void {
+      throw new Error("removeTodo is not implemented.");
+    },
+
+    updateName(name: string): void {
+      throw new Error("updateName is not implemented.");
+    },
+
+    updateDescription(description: string): void {
+      throw new Error("updateDescription is not implemented.");
+    },
+
+    markAsCompleted(completed: boolean): void {
+      throw new Error("markAsCompleted is not implemented.");
+    },
+
+    moveTodo(newIndex: number): void {
+      throw new Error("moveTodo is not implemented.");
+    },
+
+    searchTodoByName(name: string): Todo | undefined {
+      throw new Error("searchTodoByName is not implemented.");
+    },
+
+    sortTodosByName(): Todo[] {
+      throw new Error("sortTodosByName is not implemented.");
+    },
+
+    sortTodosByCreationDate(): Todo[] {
+      throw new Error("sortTodosByCreationDate is not implemented.");
+    },
+
+    saveTodosToLocalStorage(): void {
+      throw new Error("saveTodosToLocalStorage is not implemented.");
+    },
+
+    readTodosFromLocalStorage(): void {
+      throw new Error("readTodosFromLocalStorage is not implemented.");
+    },
+
+    listTodos() {
+      if (!todos.length) {
+        console.log("List is empty.");
+        return;
+      }
+
+      for (const todo of todos) {
+        console.log(todo);
+      }
+    },
+  };
+})();
+
+console.log(`Welcome to the Todo console application.
+Type "todo" in the Console to see the available functions.`);
+console.log(todo);
+window.todo = todo;
