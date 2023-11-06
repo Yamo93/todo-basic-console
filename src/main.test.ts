80fad61edf206564277bd2402ed285b572df7d2c
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2023-11-06 20:52:12 +0100

    add test

--- src/main.test.ts
+++ src/main.test.ts
@@ -4,5 +4,9 @@ import { todo } from "./main";
 test("should add a todo", () => {
   // act
   todo.addTodo("A todo", "A description");
-  expect(todo.getTodos()).toHaveLength(1);
+  const todos = todo.getTodos();
+  expect(todos).toHaveLength(1);
+  expect(todos[0].id).toBe(1);
+  expect(todos[0].name).toBe("A todo");
+  expect(todos[0].description).toBe("A description");
 });
