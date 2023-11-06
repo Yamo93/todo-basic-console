b4d519557dff513075136a98ca2c80dfb97f8580
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2023-11-06 15:39:13 +0100

    add descriptions

--- src/__global.ts
+++ src/__global.ts
@@ -4,16 +4,16 @@ declare global {
     todo: {
       addTodo: (name: string, description: string) => void;
       removeTodo: (id: number) => void;
-      updateName: (name: string) => void;
-      updateDescription: (description: string) => void;
-      listTodos: () => void;
-      markAsCompleted: (completed: boolean) => void;
-      moveTodo: (newIndex: number) => void;
+      updateName: (id: number, name: string) => void;
+      updateDescription: (id: number, description: string) => void;
+      markAsCompleted: (id: number, completed: boolean) => void;
+      moveTodo: (id: number, newIndex: number) => void;
       searchTodoByName: (name: string) => Todo | undefined;
       sortTodosByName: () => Todo[];
       sortTodosByCreationDate: () => Todo[];
       saveTodosToLocalStorage: () => void;
       readTodosFromLocalStorage: () => void;
+      listTodos: () => void;
     };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -18,50 +18,92 @@ const todo = (() => {
     },
 
     /**
-     *
+     * Removes a todo. Should throw error if the id is missing.
      * @param id Id of todo to delete
      */
     removeTodo(id: number): void {
       throw new Error("removeTodo is not implemented.");
     },
 
-    updateName(name: string): void {
+    /**
+     * Updates the name of a todo. Should throw error if the id is missing.
+     * Should throw error if name is missing.
+     * @param id Id of todo.
+     * @param name New name.
+     */
+    updateName(id: number, name: string): void {
       throw new Error("updateName is not implemented.");
     },
 
-    updateDescription(description: string): void {
+    /**
+     * Updates the description of a todo. Should throw error if the id is missing.
+     * Should throw error if description is missing.
+     * @param id Id of todo.
+     * @param description New description.
+     */
+    updateDescription(id: number, description: string): void {
       throw new Error("updateDescription is not implemented.");
     },
 
-    markAsCompleted(completed: boolean): void {
+    /**
+     * Marks a todo as completed or not. Should throw error if the id is missing.
+     * @param id Id of todo.
+     * @param completed Todo is completed.
+     */
+    markAsCompleted(id: number, completed: boolean): void {
       throw new Error("markAsCompleted is not implemented.");
     },
 
-    moveTodo(newIndex: number): void {
+    /**
+     * Moves a todo to a new index in the array. Should throw error if id is missing.
+     * Should throw error if the new index is invalid. Shifts all the todos after it.
+     * @param id Id of todo.
+     * @param newIndex New index.
+     */
+    moveTodo(id: number, newIndex: number): void {
       throw new Error("moveTodo is not implemented.");
     },
 
+    /**
+     * Searches for a todo by name.
+     * @param name Name of todo.
+     */
     searchTodoByName(name: string): Todo | undefined {
       throw new Error("searchTodoByName is not implemented.");
     },
 
+    /**
+     * Sorts todos by name alphabetically in ascending order.
+     */
     sortTodosByName(): Todo[] {
       throw new Error("sortTodosByName is not implemented.");
     },
 
+    /**
+     * Sorts todos by creation date (oldest first).
+     */
     sortTodosByCreationDate(): Todo[] {
       throw new Error("sortTodosByCreationDate is not implemented.");
     },
 
+    /**
+     * Saves todos to local storage.
+     */
     saveTodosToLocalStorage(): void {
       throw new Error("saveTodosToLocalStorage is not implemented.");
     },
 
+    /**
+     * Reads todo from local storage if such exists.
+     */
     readTodosFromLocalStorage(): void {
       throw new Error("readTodosFromLocalStorage is not implemented.");
     },
 
-    listTodos() {
+    /**
+     * Lists todos and prints them to console.
+     */
+    listTodos(): void {
       if (todos.length === 0) {
         console.log("List is empty.");
         return;
