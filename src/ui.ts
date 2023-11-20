42c136ee1c37cd24e5045dd5e0bb24aaeea7b850
Yamo Gebrewold <yage1800@student.miun.se>
2023-11-20 14:30:52 +0100

    add comment

--- src/ui.ts
+++ src/ui.ts
@@ -3,7 +3,8 @@ import { TodoInstance } from ".";
 export function draw(todo: TodoInstance) {
     const app = document.getElementById("app");
     if (!app) throw new Error("App root missing");
+    // TODO: build a ui here
     const header = document.createElement("h1");
-    header.textContent = "Build ui";
+    header.textContent = "Replace me and build UI with the todo instance";
     app.appendChild(header);
 }
